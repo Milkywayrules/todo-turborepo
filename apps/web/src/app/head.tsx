89677340a43044ac1c422e2b8bf8fac7597519d2
Dio Ilham Djatiadi <apajabole1@gmail.com>
2023-01-25 20:25:29 +0700

    feat(web): prototype web alpha

---
+++ apps/web/src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Todo App - With Turborepo</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Todo app" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
